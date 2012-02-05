7695617edb967f9de3d30c33c047b4d4acee84d5
root <root@izuz.localdomain>
2012-02-05 19:43:27 +0100

    Generate an image as status display

---
+++ display/display.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+import os
+import random
+from PIL import ImageFont, ImageDraw, Image
+from datetime import datetime
+ 
+WIDTH=240
+
+def text_centered(draw, y, text, font):
+	x = (WIDTH - font.getsize(text)[0]) / 2.0
+	draw.text((x,y), text, font=font)
+
+def create_image(path):
+	dir = os.path.dirname(os.path.abspath(__file__))
+	input_file = os.path.join(dir, "nyan.png")
+	font_file = os.path.join(dir, "ProggyClean.ttf")
+
+	im = Image.open(input_file)
+	draw = ImageDraw.Draw(im)
+
+	# Current time
+	n = datetime.now()
+	font = ImageFont.truetype(font_file, 64)
+	text_centered(draw, 12, n.strftime("%H:%M"), font)
+
+	# TODO: free disk space 
+	font = ImageFont.truetype(font_file, 32)
+	text_centered(draw, 70, "XXX MB free", font)
+
+	im.save(path, "PNG")
+
+def get_image(self):
+	path = "/tmp/image.png"
+	create_image(path)
+	return path
+
+if __name__ == "__main__":
+	create_image('test.png')
